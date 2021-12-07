7636ea64aa46a378a08a4459b28dc42b7cac2bf0
Grainfordd <ariel.abarca1234@gmail.com>
2021-12-07 19:34:19 -0300

    Agregada la funcionalidad de cambiar el fondo de pantalla y añadida la carpeta imágenes

--- juego.py
+++ juego.py
@@ -5,6 +5,9 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Juego')
 
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+color = YELLOW
 
 run = True
 while run:
@@ -15,8 +18,12 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
+            if event.key == pygame.K_UP:
+                color = YELLOW
+            if event.key == pygame.K_DOWN:
+                color = BLUE
 
-    screen.fill(pygame.Color('green'))
+    screen.fill(pygame.Color(color))
     pygame.display.flip()
 
 pygame.quit()
